974747161db1b63b0a3589628b731b8bc8d17718
mesut <mesut.tasci@otelz.com>
2024-04-25 12:34:18 +0300

    freecancel added, colored and detailed

--- src/timeline_penalty/script.js
+++ src/timeline_penalty/script.js
@@ -65,11 +65,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var currentDate = new Date();
     setCurrentDateTimeInput("current-date-time", currentDate);
     var checkinDate = new Date();
-    checkinDate.setDate(currentDate.getDate());
+    checkinDate.setDate(currentDate.getDate()+10);
     setCurrentDateTimeInput("checkin-date", checkinDate, true);
 });
 
-function addPoint(id, date, message, bgColor) {
+function addPoint(timeline, id, date, message, bgColor) {
     const point = document.createElement("div");
     point.classList.add("point");
     point.id = id;
@@ -97,6 +97,16 @@ function addPoint(id, date, message, bgColor) {
 
 
 function updateTimeline() {
+    const freeCancelCheckbox = document.getElementById('free-cancel');
+    const cancelPolicy = document.getElementById('cancel-policy');
+
+    if (freeCancelCheckbox.checked) {
+        cancelPolicy.style.display = 'none';
+    } else {
+        cancelPolicy.style.display = 'block';
+    }
+
+
     const currentDate = new Date(document.getElementById("current-date-time").value);
     const checkinDate = new Date(document.getElementById("checkin-date").value);
     const checkinDateTime = getCheckinDateTime();
@@ -105,58 +115,55 @@ function updateTimeline() {
 
     const timeline = document.getElementById("timeline");
     timeline.innerHTML = "";
+    const timeline2 = document.getElementById("timeline2");
+    timeline2.innerHTML = "";
 
     // A current-date-time
-    addPoint("A", currentDate, "Rezervasyon Saati");
+    addPoint(timeline, "A", currentDate, "Rezervasyon Saati");
+    addPoint(timeline2, "A", currentDate, "Rezervasyon Saati");
 
     // B current-date-time + anlik-iptal-saati
-    const pointBDate = new Date(currentDate);
-    pointBDate.setHours(currentDate.getHours() + parseInt(document.getElementById("anlik-iptal-saati").value));
-            //addPoint("B", pointBDate, "Anlık Ücretsiz İptal Saati");
+    const pointInstantCancel = new Date(currentDate);
+    pointInstantCancel.setHours(currentDate.getHours() + parseInt(document.getElementById("anlik-iptal-saati").value));
 
     // C eğer current-date-time == checkin-date-time ise current-date-time.date + ayni-gun-giris-saati - 1dakika
-    const pointCDate = new Date(checkinDate);
-    pointCDate.setHours(checkinDate.getUTCHours() + parseInt(document.getElementById("ayni-gun-giris-saati").value));
-        // pointCDate.setMinutes(checkinDate.getMinutes() - 1); // 1 dakika çıkar
-            //addPoint("C", pointCDate, "Aynı Gün Giriş Ücretsiz İptal Saati");
+    const pointSameDay = new Date(currentDate);
+    pointSameDay.setHours(0, 0, 0, 0);
+    if (currentDate.toDateString() === checkinDate.toDateString()) {
+        pointSameDay.setHours(checkinDate.getUTCHours() + parseInt(document.getElementById("ayni-gun-giris-saati").value));
+            // pointCDate.setMinutes(checkinDate.getMinutes() - 1); // 1 dakika çıkar
+    }
 
     // E checkin-date-time.date - varis-gun-input gün + 14 saat
-    const pointEDate = new Date(checkinDate);
-    pointEDate.setDate(checkinDate.getDate() - parseInt(freeCancelToDate));
-    pointEDate.setHours(pointEDate.getUTCHours() + parseInt(document.getElementById("varis-gun-saati").value));
+    var pointArrivalDate = new Date(checkinDate);
+    pointArrivalDate.setDate(pointArrivalDate.getDate() - parseInt(freeCancelToDate));
+    pointArrivalDate.setHours(pointArrivalDate.getUTCHours() + parseInt(document.getElementById("varis-gun-saati").value));
+   
+    if (freeCancelCheckbox.checked) { pointArrivalDate = checkinDateTime; }
 
-            // addPoint(">", checkinDateTime, "Check-in Saati");
-    if (currentDate.toDateString() === checkinDate.toDateString()) {
-        const points = [
-            { id: "B", date: pointBDate, label: "Anlık Ücretsiz İptal Saati", bgColor: "green"},
-            { id: "C", date: pointCDate, label: "Aynı Gün Giriş Ücretsiz İptal Saati", bgColor: "green"},
-            { id: "E", date: pointEDate, label: "Varış Günü Ücretsiz İptal Saati", bgColor: "green"},
-        ];
-        const maxDate = Math.max(...points.map(point => point.date.getTime()));
-        const maxPoint = points.find(point => point.date.getTime() === maxDate);
+    const points = [
+        { id: "B", date: pointInstantCancel, label: "Anlık Ücretsiz İptal Saati", bgColor: "green"},
+        { id: "C", date: pointSameDay, label: "Aynı Gün Giriş Ücretsiz İptal Saati", bgColor: "green"},
+        { id: "D", date: pointArrivalDate, label: "Varış Günü Ücretsiz İptal Saati", bgColor: "green"},
+    ];
 
-        if (maxPoint.date > currentDate && maxPoint.date < checkinDateTime){
-            addPoint(maxPoint.id, maxPoint.date, maxPoint.label, maxPoint.bgColor);
+    const maxDate = Math.max(...points.map(point => point.date.getTime()));
+    const maxPoint = points.find(point => point.date.getTime() === maxDate);
+
+    if (maxPoint.date > currentDate && maxPoint.date < checkinDateTime){
+        addPoint(timeline, maxPoint.id, maxPoint.date, maxPoint.label, maxPoint.bgColor);
+    }
+    if(checkinDateTime > currentDate){
+        if(maxPoint.date >= checkinDateTime) {
+            addPoint(timeline, maxPoint.id, checkinDateTime, maxPoint.label + " >= Check-in Saati", maxPoint.bgColor);
         }
-        if(checkinDateTime > currentDate){
-            if(maxPoint.date >= checkinDateTime) {
-                addPoint(maxPoint.id, checkinDateTime, maxPoint.label + " >= Check-in Saati", maxPoint.bgColor);
-            }
-            else{
-                addPoint("Z", checkinDateTime, "Check-in Saati", "orange");
-            }
+        else{
+            addPoint(timeline, "Z", checkinDateTime, "Check-in Saati", "orange");
         }
     }
-    else {
-        addPoint("B", pointBDate, "Anlık Ücretsiz İptal Saati");
-        addPoint("E", pointEDate);
     
-        // D checkin-date-time + varis-gun-saati
-        const pointDDate = new Date(checkinDate);
-        pointDDate.setHours(checkinDate.getUTCHours() + parseInt(document.getElementById("varis-gun-saati").value));
-        addPoint("D", pointDDate, "Varış Günü Saati (Sonrası NoShow)");
-        addPoint("Z", checkinDateTime, "Check-in Saati", "orange");
-    }
+    points.forEach(point => addPoint(timeline2, point.id, point.date, point.label, point.bgColor));
+    addPoint(timeline2, "Z", checkinDateTime, "Check-in Saati", "orange");
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -165,4 +172,7 @@ document.addEventListener("DOMContentLoaded", function() {
     inputFields.forEach(input => {
         input.addEventListener('change', updateTimeline);
     });
+    
+    const freeCancelCheckbox = document.getElementById('free-cancel');
+    freeCancelCheckbox.addEventListener('change',  updateTimeline);
 });
